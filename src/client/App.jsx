d6adea6f7ee12ba815e78d74ad0a09d121aaec20
Eric McCorkle <ericm.mccorkle@gmail.com>
2022-05-29 14:10:32 -0700

    completed project template for real

---
+++ src/client/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
+
+export default App;
